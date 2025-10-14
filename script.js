6e9749054bd4eacdee6bc623092d03df2e6313c9
TeNgO-rgb <ttskhvedadze349@gmail.com>
2025-10-14 15:49:08 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
 
-const links = document.querySelectorAll('.nav-link');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        links.forEach(l => l.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
-
-
-window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('section');
-    const scrollPos = window.scrollY + 100;
-
-    sections.forEach(section => {
-        if (scrollPos >= section.offsetTop && scrollPos < section.offsetTop + section.offsetHeight) {
-            links.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href') === `#${section.id}`) {
-                    link.classList.add('active');
-                }
-            });
-        }
-    });
+burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    burger.classList.toggle("toggle");
 });
