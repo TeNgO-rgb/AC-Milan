855d48ce8055cf6329fc280cc825b475053b852a
TeNgO-rgb <ttskhvedadze349@gmail.com>
2025-10-20 16:24:35 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
 
-burger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    burger.classList.toggle("toggle");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    if (form) {
+        form.addEventListener("submit", function (e) {
+            const name = document.getElementById("name").value.trim();
+            const surname = document.getElementById("surname").value.trim();
+            const email = document.getElementById("email").value.trim();
+            const phone = document.getElementById("phone").value.trim();
+
+            if (!name || !surname || !email || !phone) {
+                alert("გთხოვთ შეავსეთ ყველა ველი!");
+                e.preventDefault();
+            } else {
+                alert("მადლობა! თქვენი მონაცემები მიღებულია.");
+            }
+        });
+    }
 });
+
+
+const carCards = document.querySelectorAll(".car-card");
+carCards.forEach(card => {
+    card.addEventListener("click", () => {
+        window.location.href = "contact.html";
+    });
+});
+
+
+    function toggleMenu() {
+    const nav = document.querySelector('.nav-links');
+    nav.classList.toggle('active');
+  }
+
+
+
+function searchFunction() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const items = document.querySelectorAll("#resultsList li");
+
+    items.forEach(item => {
+        const text = item.textContent.toLowerCase();
+        item.style.display = text.includes(input) ? "list-item" : "none";
+    });
+}
